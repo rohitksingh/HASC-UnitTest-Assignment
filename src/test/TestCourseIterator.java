d723cc2bf9733c89b68c379ae8741c313f819528
Rohit <r4rohit002@gmail.com>
2019-10-29 22:36:19 -0700

    Added

---
+++ src/test/TestCourseIterator.java
@@ -0,0 +1,32 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import hacs.ClassCourseList;
+import hacs.CourseIterator;
+
+@DisplayName("Test Course Iterator")
+class TestCourseIterator {
+
+	CourseIterator courseIterator;
+	ClassCourseList courseList;
+	
+	@BeforeEach
+	public void init() {
+		
+		courseIterator = new CourseIterator();
+		courseList = new ClassCourseList();
+		
+		
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
