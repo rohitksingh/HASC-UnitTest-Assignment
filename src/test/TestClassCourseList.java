242fb5c9054248cb82c802ea33e13691fa9b3bd1
Rohit <r4rohit002@gmail.com>
2019-11-01 16:52:34 -0700

    Added file

--- src/test/TestClassCourseList.java
+++ src/test/TestClassCourseList.java
@@ -1,6 +1,5 @@
 package test;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
